f6fa839be32f1e2e9cbafb6f48e0277cda3ab2c7
adrianperkins369 <86713638+adrianperkins369@users.noreply.github.com>
2025-05-12 22:40:50 +0000

    Deploying to gh-pages from @ adrianperkins369/adrianperkins369.github.io@319bf2b36fc5f9afeeffdee95bb4107157c1a402 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/documentation/";
+            window.location.href = "/documentation/index.html";
           },
         },{id: "nav-repositories",
           title: "repositories",
