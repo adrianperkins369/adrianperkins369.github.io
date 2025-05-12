90a63d0c32a631ea3a10850c6c1f9ae145f1223b
adrianperkins369 <86713638+adrianperkins369@users.noreply.github.com>
2025-05-12 23:02:15 +0000

    Deploying to gh-pages from @ adrianperkins369/adrianperkins369.github.io@c5888583bfa54554e80f6d55f18857657849ea8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,7 +452,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%64%72%69%61%6E%70%65%72%6B%69%6E%73%33%36%39@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
