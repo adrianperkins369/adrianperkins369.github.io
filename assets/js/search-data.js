368a8d387f92dd5014281156efa80083ea8b2382
adrianperkins369 <86713638+adrianperkins369@users.noreply.github.com>
2025-05-14 07:04:19 +0000

    Deploying to gh-pages from @ adrianperkins369/adrianperkins369.github.io@988af41d807d255f195833b6272c777e43a8e88b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -481,6 +481,27 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%61%64%72%69%61%6E%70%65%72%6B%69%6E%73%33%36%39@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/", "_blank");
+        },
+      },{
+        id: 'social-medium',
+        title: 'Medium',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://medium.com/@", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
